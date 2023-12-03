4eb4b054c91bdcdfdf195d164f2fbe4c4d6bf02f
Muhammad bilal <meharbilal24525@gmail.com>
2023-12-03 13:12:16 +0500

    Add project files.

---
+++ Class And Object/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Class_And_Object
+{
+    class Program
+    {
+        int RollNo, age, standard;
+        string name;
+        public void SetStudent(int rollNumber, string name, int age, int standard)
+        {
+            this.RollNo = rollNumber;
+            this.name = name;
+            this.age = age;
+            this.standard = standard;
+        }
+        public void GetStudent()
+        {
+            Console.WriteLine("Roll No is : {0}", this.RollNo);
+            Console.WriteLine("Name is : {0}", this.name);
+            Console.WriteLine("Age is : {0}", age);
+            Console.WriteLine("Standard is : {0}", this.standard);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program Ali = new Program();
+            Ali.SetStudent(1, "Ali", 22, 15);
+            Ali.GetStudent();
+
+
+            Console.WriteLine("-----------------");
+            Program Bilal = new Program();
+            Bilal.SetStudent(2, "Bilal", 24, 16);
+            Bilal.GetStudent();
+            Console.ReadLine();
+
+        }
+    }
+}
